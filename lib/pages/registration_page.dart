f9c2087872fc232723ef14048f641b5f058533b6
Kulbayev <damir.kulbayev@mail.ru>
2022-10-12 23:05:11 +0600

    registration ui demo

---
+++ lib/pages/registration_page.dart
@@ -0,0 +1,376 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+import 'package:registration_ui/pages/welcome_page.dart';
+import '../model/user.dart';
+
+class RegistrationPage extends StatefulWidget {
+  const RegistrationPage({Key? key}) : super(key: key);
+
+  @override
+  State<RegistrationPage> createState() => _RegistrationPageState();
+}
+
+class _RegistrationPageState extends State<RegistrationPage> {
+  bool _hidepassword = true;
+
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  final _formKey = GlobalKey<FormState>();
+
+  final _fullnameController = TextEditingController();
+  final _usernameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
+  final _passController = TextEditingController();
+
+  final _fullnameFocus = FocusNode();
+  final _usernameFocus = FocusNode();
+  final _emailFocus = FocusNode();
+  final _phoneFocus = FocusNode();
+  final _passFocus = FocusNode();
+
+  final maskFormatter = MaskTextInputFormatter(mask: '+7(###)###-##-##');
+
+  User newUser = User();
+
+  void _fieldFocusChange(
+      BuildContext context, FocusNode currentFocus, FocusNode nextFocus) {
+    currentFocus.unfocus();
+    FocusScope.of(context).requestFocus(nextFocus);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        key: _scaffoldKey,
+        body: Form(
+          key: _formKey,
+          child: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Color(0xFF496AE1),
+                    Color(0xFFCE48B1),
+                  ]),
+            ),
+            child: ListView(
+              padding: const EdgeInsets.only(
+                  top: 300, bottom: 250, right: 40, left: 40),
+              children: [
+                TextFormField(
+                  focusNode: _fullnameFocus,
+                  autofocus: true,
+                  onFieldSubmitted: (_) {
+                    _fieldFocusChange(context, _fullnameFocus, _usernameFocus);
+                  },
+                  controller: _fullnameController,
+                  decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xFFB39DDB),
+                      labelText: "Full Name",
+                      hintText: "Enter Full Name",
+                      prefixIcon: Icon(
+                        Icons.people,
+                        color: Color(0xFF9575CD)),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        borderSide:
+                            BorderSide(color: Colors.transparent, width: 2.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        borderSide:
+                            BorderSide(color: Color(0xFF9575CD), width: 2.0),
+                      )),
+                  validator: validateFullName,
+                  onSaved: (value) => newUser.fullname = value!,
+                ),
+
+                SizedBox(height: 7),
+
+                TextFormField(
+                  focusNode: _usernameFocus,
+                  autofocus: true,
+                  onFieldSubmitted: (_) {
+                    _fieldFocusChange(context, _usernameFocus, _emailFocus);
+                  },
+                  controller: _usernameController,
+                  decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xFFB39DDB),
+                      labelText: "Username",
+                      hintText: "Enter username",
+                      prefixIcon: Icon(
+                        Icons.assignment_ind,
+                        color: Color(0xFF9575CD),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        borderSide:
+                            BorderSide(color: Colors.transparent, width: 2.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        borderSide:
+                            BorderSide(color: Color(0xFF9575CD), width: 2.0),
+                      )),
+                  validator: validateUsername,
+                  onSaved: (value) => newUser.username = value!,
+                ),
+
+                SizedBox(height: 7),
+
+                TextFormField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xFFB39DDB),
+                      labelText: "Email address",
+                      hintText: "Enter email address",
+                      prefixIcon: Icon(
+                        Icons.alternate_email,
+                        color: Color(0xFF9575CD),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        borderSide:
+                            BorderSide(color: Colors.transparent, width: 2.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        borderSide:
+                            BorderSide(color: Color(0xFF9575CD), width: 2.0),
+                      )),
+                  keyboardType: TextInputType.emailAddress,
+                  // validator: validateEmail,
+                  onSaved: (value) => newUser.email = value!,
+
+                ),
+
+                SizedBox(height: 7),
+
+                TextFormField(
+                  focusNode: _phoneFocus,
+                  onFieldSubmitted: (_) {
+                    _fieldFocusChange(context, _phoneFocus, _passFocus);
+                  },
+                  controller: _phoneController,
+                  // maxLength: 10,
+                  decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xFFB39DDB),
+                      labelText: "Phone number",
+                      hintText: "Phone format: (XXX)XXX-XXXX",
+                      prefixIcon: Icon(
+                        Icons.phone,
+                        color: Color(0xFF9575CD),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        borderSide:
+                            BorderSide(color: Colors.transparent, width: 2.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        borderSide:
+                            BorderSide(color: Color(0xFF9575CD), width: 2.0),
+                      )),
+                  keyboardType: TextInputType.phone,
+                  // inputFormatters: [
+                  //   maskFormatter,
+                  //   FilteringTextInputFormatter(RegExp(r'^[()\d -]{1,10}$'),
+                  //       allow: true),
+                  // ],
+                  inputFormatters: [
+                    maskFormatter],
+                  // validator: (value) => validatePhoneNumber(value!)
+                  //     ? null
+                  //     : 'Phone number must be entered as (###)###-####',
+                  onSaved: (value) => newUser.phone = value!,
+                ),
+
+                SizedBox(height: 7),
+
+                TextFormField(
+                  focusNode: _passFocus,
+                  controller: _passController,
+                  obscureText: _hidepassword,
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xFFB39DDB),
+                      labelText: "Password",
+                      hintText: "Enter password",
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                            _hidepassword
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                            color: Color(0xFF9575CD)),
+                        onPressed: () {
+                          setState(() {
+                            _hidepassword = !_hidepassword;
+                          });
+                        },
+                      ),
+                      prefixIcon: const Icon(
+                        Icons.password,
+                        color: Color(0xFF9575CD),
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        borderSide:
+                            BorderSide(color: Colors.transparent, width: 2.0),
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        borderSide:
+                            BorderSide(color: Color(0xFF9575CD), width: 2.0),
+                      )),
+                  validator: _validatePassword,
+                ),
+
+                SizedBox(height: 7),
+
+                ElevatedButton(
+                  onPressed: _submitForm,
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xFF7E57C2),
+                    textStyle: TextStyle(color: Color(0xFF9575CD)),
+                    fixedSize: const Size(20,10),
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(32.0))
+                      ),
+                  ),
+                  child: const Text('Registration'),
+                  //color: Colors.green,
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      _showDialog(name: _fullnameController.text);
+    } else {
+      _showMessage(message: 'Form is not valid! Please review and correct');
+    }
+  }
+
+  String? validateFullName(String? value) {
+    final nameExp = RegExp(r'^[A-Za-z ]+$');
+    if (value == null) {
+      return 'Fullname is reqired.';
+    } else if (!nameExp.hasMatch(value)) {
+      return 'Please enter alphabetical characters.';
+    } else {
+      return null;
+    }
+  }
+
+  String? validateUsername(String? value) {
+    final nameExp = RegExp(r'^[A-Za-z ]+$');
+    if (value == null) {
+      return 'Username is reqired.';
+    } else if (!nameExp.hasMatch(value)) {
+      return 'Please enter alphabetical characters.';
+    } else {
+      return null;
+    }
+  }
+
+String? validateEmail(String? value) {
+  if (value == null) {
+    return 'Email cannot be empty';
+  } else if (!_emailController.text.contains('@')) {
+    return 'Invalid email address';
+  } else {
+    return null;
+  }
+}
+
+  // bool validatePhoneNumber(String input) {
+  //   final phoneExp = RegExp(r'^\(\d\d\d\)\d\d\d\-\d\d\-\d\d$');
+  //   return phoneExp.hasMatch(input);
+  // }
+
+String? _validatePassword(String? value) {
+  if (_passController.text.length <= 7) {
+    return '8 character required for password';
+  }  else {
+    return null;
+  }
+}
+  void _showDialog({required String name}) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(32.0))
+          ),
+          title: const Text(
+            'Done',
+            style: TextStyle(
+              color: Color(0xFF9575CD),
+              fontFamily: 'helvetica_neue_light',
+              fontSize: 23
+            ),
+          ),
+          content: Text(
+            '$name good job',
+            style: const TextStyle(
+              fontWeight: FontWeight.w700,
+              fontSize: 18.0,
+              fontFamily: 'helvetica_neue_light',
+            ),
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => WelcomePage(
+                      usersInfo: newUser,
+                    ),
+                  ),
+                );
+              },
+              child: const Text(
+                'Continue',
+                style: TextStyle(
+                  color: Color(0xFF9575CD),
+                  fontSize: 18.0,
+                  fontFamily: 'helvetica_neue_light',
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showMessage({required String message}) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 1),
+        backgroundColor: Colors.red,
+        content: Text(
+          message,
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+            fontSize: 18.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../model/user.dart';
+
+class WelcomePage extends StatelessWidget {
+
+  final User usersInfo;
+  const WelcomePage({Key? key, required this.usersInfo}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text("dsdsadsa"),
+    );
+  }
+}
