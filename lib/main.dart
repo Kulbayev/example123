3ea2ca8ec5386fa405d0b3b3c85eedac867755b8
Kulbayev <damir.kulbayev@mail.ru>
2022-10-13 19:19:17 +0600

    splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:registration_ui/model/user.dart';
 import 'package:registration_ui/pages/registration_page.dart';
 import 'package:registration_ui/pages/logginig_page.dart';
+import 'package:page_transition/page_transition.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -17,7 +21,40 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const RegistrationPage(),
+      home: const SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Lottie.asset('assets/images/splash.json'),
+      backgroundColor: Color(0xFF93CBF5),
+      nextScreen: const RegistrationPage(),
+      splashIconSize: 250,
+      duration: 2900,
+      splashTransition: SplashTransition.fadeTransition,
+      pageTransitionType: PageTransitionType.leftToRightWithFade,
+      animationDuration: const Duration(seconds: 1),
+    );
+
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        centerTitle: true,
+      ),
     );
   }
 }
